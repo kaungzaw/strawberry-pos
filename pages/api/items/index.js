fcfee91b2ba1ef68df626c9b28c7ff6935cdd008
Kaung Zaw <kaungzaw@immin.io>
2022-10-10 09:22:52 +0630

    Remove unnecessary GET method

--- pages/api/items/index.js
+++ pages/api/items/index.js
@@ -6,15 +6,7 @@ import { create } from "@lib/crud";
 async function handler(req, res) {
   await dbConnect();
 
-  if (req.method === "GET") {
-    try {
-      const result = await Item.find();
-      res.status(200).json(result);
-    } catch (error) {
-      console.log(error);
-      res.status(400).end();
-    }
-  } else if (req.method === "POST") {
+  if (req.method === "POST") {
     try {
       const result = await create(Item, req.body);
       res.status(201).json(result);
--- pages/api/sales/index.js
+++ pages/api/sales/index.js
@@ -7,15 +7,7 @@ import { create } from "@lib/crud";
 async function handler(req, res) {
   await dbConnect();
 
-  if (req.method === "GET") {
-    try {
-      const result = await Sale.find();
-      res.status(200).json(result);
-    } catch (error) {
-      console.log(error);
-      res.status(400).end();
-    }
-  } else if (req.method === "POST") {
+  if (req.method === "POST") {
     try {
       const { itemId, quantity } = req.body;
       await create(Sale, req.body);
