91b4d52ce204c607bcb1eae26ccea62c5b3a8cdc
Kaung Zaw <kaungzaw@immin.io>
2022-10-10 10:04:54 +0630

    Add progress bar on page change

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,20 @@
 import "../styles/globals.css";
-// import { useState } from "react";
+import "../styles/nprogress.css";
+import NProgress from "nprogress";
 import MainLayout from "components/MainLayout";
-// import Spinner from "components/Spinner";
 
 function MyApp({ Component, pageProps, ...rest }) {
   const { router } = rest;
 
-  // const [loading, setLoading] = useState();
-
-  // const toggleLoading = () => setLoading((loading) => !loading);
-
-  // router.events?.on("routeChangeStart", toggleLoading);
-  // router.events?.on("routeChangeComplete", toggleLoading);
+  router.events?.on("routeChangeStart", () => NProgress.start());
+  router.events?.on("routeChangeComplete", () => NProgress.done());
+  router.events?.on("routeChangeError", () => NProgress.done());
 
   if (["/auth/login"].includes(router.pathname))
     return <Component {...pageProps} />;
 
   return (
     <MainLayout>
-      {/* {loading ? <Spinner /> : <Component {...pageProps} />} */}
       <Component {...pageProps} />
     </MainLayout>
   );
